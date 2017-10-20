56d3f08b23c1780df33b45e974e622023d24ee57
Jan-Peter Alten <alten@alten.de>
2017-10-20 07:57:46 +0200

    added (SSD 1306) display

--- sole.ino
+++ sole.ino
@@ -1,12 +1,32 @@
 #include "TSIC.h"       // include the library
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define OLED_RESET 4
+Adafruit_SSD1306 display(OLED_RESET);
+
+#define NUMFLAKES 10
+#define XPOS 0
+#define YPOS 1
+#define DELTAY 2
+
+
+#define LOGO16_GLCD_HEIGHT 16
+#define LOGO16_GLCD_WIDTH  16
+
+#if (SSD1306_LCDHEIGHT != 64)
+#error("Height incorrect, please fix Adafruit_SSD1306.h!");
+#endif
 
 // instantiate the library, representing the sensor
-TSIC Sensor1(4, 2);    // Signalpin, VCCpin, Sensor Type
+TSIC Sensor1(3, 2);    // Signalpin, VCCpin, Sensor Type
 //TSIC Sensor1(4);    // only Signalpin, VCCpin unused by default
 
 uint16_t temperature = 0;
 float Temperatur_C = 0;
-float temp_high_off = 28;
+float temp_high_off = 24;
 float temp_low_off = 7;
 float histeresis = 0.5;
 
@@ -28,6 +48,9 @@ void setup() {
   pinMode(pumpPin, OUTPUT);
   digitalWrite(pumpPin, pumpOff);
   switchTimer = 1000;
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);  // initialize with the I2C addr 0x3D (for the 128x64)
+  display.display();
+  display.clearDisplay();
 }
 
 void loop() {
@@ -62,6 +85,27 @@ void loop() {
   // set the LED with the ledState of the variable:
   digitalWrite(ledPin, ledState);
 
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0, 0);
+  display.println("Aussentemperatur:");
+  display.setTextSize(2);
+  display.println(Temperatur_C);
+  display.setTextSize(1);
+  display.println("");
+  display.println("Pumpe:");
+  display.setTextSize(2);
+  if (currentPunmpState == pumpOff)
+  {
+    display.println("Aus");
+  }
+  else
+  {
+    display.println("An");
+  }
+
+  display.display();
   delay(switchTimer);
 
 
