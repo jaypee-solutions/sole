0584914f7853d7d79bad6a685b9706c17a3cb130
Jan-Peter Alten <alten@alten.de>
2017-10-22 19:42:54 +0200

    added switch to turn display off, cleanup

--- sole.ino
+++ sole.ino
@@ -1,28 +1,24 @@
 #include "TSIC.h"       // include the library
 #include <SPI.h>
 #include <Wire.h>
-#include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 
 #define OLED_RESET 4
 Adafruit_SSD1306 display(OLED_RESET);
 
-#define NUMFLAKES 10
-#define XPOS 0
-#define YPOS 1
-#define DELTAY 2
-
-
-#define LOGO16_GLCD_HEIGHT 16
-#define LOGO16_GLCD_WIDTH  16
-
 #if (SSD1306_LCDHEIGHT != 64)
 #error("Height incorrect, please fix Adafruit_SSD1306.h!");
 #endif
 
-// instantiate the library, representing the sensor
 TSIC Sensor1(3, 2);    // Signalpin, VCCpin, Sensor Type
 //TSIC Sensor1(4);    // only Signalpin, VCCpin unused by default
+
+const int ledPin =  LED_BUILTIN;
+const int buttonPin = 4;
+const int pumpOn = LOW;
+const int pumpOff = HIGH;
+const int pumpPin = 10;
+const int switchDelay = 900;
 
 uint16_t temperature = 0;
 float Temperatur_C = 0;
@@ -30,25 +26,20 @@ float temp_high_off = 24;
 float temp_low_off = 7;
 float histeresis = 0.5;
 
-const int ledPin =  LED_BUILTIN;// the number of the LED pin
-const int pumpOn = LOW;
-const int pumpOff = HIGH;
-const int pumpPin = 10;
-
-// Variables will change:
-int ledState = LOW;             // ledState used to set the LED
+int ledState = LOW;
+int buttonState = 0;
 int currentPunmpState = pumpOff;
 int targetPunmpState = pumpOff;
-
-int switchTimer = 900000;
+int switchTimer = 0;
 
 void setup() {
-  Serial.begin(9600); // set up the serial port
+  Serial.begin(9600);
   pinMode(ledPin, OUTPUT);
+  pinMode(buttonPin, INPUT);
+  digitalWrite(buttonPin, HIGH);
   pinMode(pumpPin, OUTPUT);
   digitalWrite(pumpPin, pumpOff);
-  switchTimer = 1000;
-  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);  // initialize with the I2C addr 0x3D (for the 128x64)
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
   display.display();
   display.clearDisplay();
 }
@@ -60,11 +51,13 @@ void loop() {
     Serial.print(Temperatur_C);
     Serial.println(" °C");
   }
+
   if (ledState == LOW) {
     ledState = HIGH;
   } else {
     ledState = LOW;
   }
+  digitalWrite(ledPin, ledState);
 
   if (Temperatur_C <= (temp_low_off - histeresis)) {
     targetPunmpState = pumpOn;
@@ -78,36 +71,51 @@ void loop() {
   }
 
   if (targetPunmpState != currentPunmpState) {
+    if (switchTimer >= switchDelay){
     currentPunmpState = targetPunmpState;
+    switchTimer = 0;
     digitalWrite(pumpPin, currentPunmpState);
+    }
+    else
+    {
+      switchTimer++;
+    }
   }
 
-  // set the LED with the ledState of the variable:
-  digitalWrite(ledPin, ledState);
+  buttonState = digitalRead(buttonPin);
+  if (buttonState == HIGH) {
+    display.ssd1306_command(SSD1306_DISPLAYON);
+    display.clearDisplay();
+    display.setTextSize(1);
+    display.setTextColor(WHITE);
+    display.setCursor(0, 0);
+    display.println("Aussentemperatur:");
+    display.setTextSize(2);
+    display.println(Temperatur_C);
+    display.setTextSize(1);
+    display.println("");
+    display.println("Pumpe:");
+    display.setTextSize(2);
 
-  display.clearDisplay();
-  display.setTextSize(1);
-  display.setTextColor(WHITE);
-  display.setCursor(0, 0);
-  display.println("Aussentemperatur:");
-  display.setTextSize(2);
-  display.println(Temperatur_C);
-  display.setTextSize(1);
-  display.println("");
-  display.println("Pumpe:");
-  display.setTextSize(2);
-  if (currentPunmpState == pumpOff)
-  {
-    display.println("Aus");
+    if (currentPunmpState == pumpOff)
+    {
+      display.println("Aus");
+    }
+    else
+    {
+      display.println("An");
+    }
+    display.setTextSize(1);
+    display.print(switchTimer);
+    display.print("/");
+    display.print(switchDelay);
+    display.display();
+  } else {
+    display.ssd1306_command(SSD1306_DISPLAYOFF);
   }
-  else
-  {
-    display.println("An");
-  }
 
-  display.display();
-  delay(switchTimer);
-
-
+  delay(1000);
+  //
+  //delay(switchTimer);
 }
 
